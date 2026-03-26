0330a73a24dd42cc1a9a2e812c4978775a86206d
suport@foxmail.com <89622702@qq.com>
2026-03-26 11:37:37 +0800

    more

---
+++ controllers/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+from . import report_server
+from . import ir_actions_report
+from . import custom_file_controller
---
+++ models/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+
+from . import report_builder,report
+from . import report_font
